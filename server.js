911ee744fcf599e814854f029e19bb27d6581034
ngard1234 <neilgardner@verizon.net>
2022-11-19 11:26:43 -0500

    create and new routes complete

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const app = express()
   app.set('view engine', 'jsx')
   app.engine('jsx', require('express-react-views').createEngine())
   app.use(express.static('public'))
+  app.use(express.urlencoded({extended: true}))
+
   
 
 // ROUTES
--- views/index.jsx
+++ views/index.jsx
@@ -6,6 +6,7 @@ function Index ({breads, title}) {
       <Default title =  {title}>
        {/*<h2>Index Page</h2>*/}
        {/*<p>I have {breads[0].name} bread!</p>*/}
+       
        <ul>
         {
             breads.map((bread,index) => {
@@ -19,6 +20,11 @@ function Index ({breads, title}) {
             })
         }
        </ul>
+
+       <div className="newButton">
+         <a href="/breads/new"><button>Add a new bread</button></a>
+       </div>
+
       </Default>
     )
 }
